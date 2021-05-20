44734e27142d2253d3abab11afc8d8bc36e2003c
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2021-05-20 17:13:16 -0500

    saludo

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
-
-nombre = input('Ingresa tu nombre por favor: \n')
-print(f'hola {nombre} que tal maquinola')
-
+def saludo(nombre):
+    return print(f'Hola {nombre}')
+nombre= input('ingresa tu nombre')
+saludo(nombre)
 
 
