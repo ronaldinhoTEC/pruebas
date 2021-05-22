53f1cb099a57300f424cf9dc16e42192db48d3a1
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2021-05-21 23:59:05 -0500

    logica agregada

--- prueba.py
+++ prueba.py
@@ -1,12 +1,34 @@
-<<<<<<< HEAD
-=======
 from tkinter import *
 
-raiz = Tk()
-raiz.title('Estamos haciendo las pruebas correspondientes en la app')
+root = Tk()
+#raiz.title('Mi app')
+#raiz.geometry('330x200')
+#raiz.config(bg='black')
+
+#mi_frame= Frame()
+#mi_frame.pack(fill='both', expand='True')
+#mi_frame.config(bg='gold')
+#mi_frame.config(width='450', height='250')
+#mi_frame.config(relief='groove')
+
+mi_frame= Frame(root, width=500, height=400)
+mi_frame.pack()
+
+nombre = input('Ingresa tu nombre por favor: \n')
+
+frase= f'Hola {nombre} como estas maquinola'
+nombres=['Ronal','Gabriel','Milagros','Cristina','Marcela']
+for i in nombres:
+    if nombre==i:
+        Label(mi_frame, text=frase).place(x=10, y=10)
+        print(frase)
+        break
+    else:
+        print(f'{nombre} no esta registrado en nuestra base de datos \n'
+              f'Logueate en nuestra base de datos')
+        break
 
 
+root.mainloop()
 
-raiz.mainloop()
->>>>>>> a7c75e4b4057d69ee8ee44b606f5af2489eeda6c
 
